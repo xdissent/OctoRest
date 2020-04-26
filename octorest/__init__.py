e0055ff3d3b777fb3c9f362e249c38755d9fba82
Yarin <15931306+TheYarin@users.noreply.github.com>
2020-04-26 17:47:28 +0300

    Updated __init__.py to include the new AuthorizationRequestPollingResult enum

--- octorest/__init__.py
+++ octorest/__init__.py
@@ -1,8 +1,8 @@
-from .client import OctoRest
+from .client import OctoRest, AuthorizationRequestPollingResult
 from .xhrstreaminggenerator import XHRStreamingGenerator
 from .xhrstreaming import XHRStreamingEventHandler
 from .websocket import WebSocketEventHandler
 
 
-__all__ = ['OctoRest', 'XHRStreamingGenerator',
+__all__ = ['OctoRest','AuthorizationRequestPollingResult' , 'XHRStreamingGenerator',
            'XHRStreamingEventHandler', 'WebSocketEventHandler']
